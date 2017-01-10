a3945c845e69bfbc4c031e0ee401c4efd9fc711f
tylerstephens814 <tylerstephens814@gmail.com>
2017-01-10 10:29:54 -0700

    Completed the protfolios contorller. added stronf params

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,11 +4,11 @@ class PortfoliosController < ApplicationController
   end
 
   def new
-    @portfolio_items = Portfolio.new
+    @portfolio_item = Portfolio.new
   end
       
     def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
       respond_to do |format|
         if @portfolio_item.save
@@ -18,5 +18,35 @@ class PortfoliosController < ApplicationController
         end
       end
     end
+
+    def show
+      @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def edit
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def update
+      @portfolio_item = Portfolio.find(params[:id])
+      if @portfolio_item.update(portfolio_params)
+        redirect_to @portfolio_item
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @portfolio_item = Portfolio.find(params[:id])
+      @portfolio_item.destroy
+      redirect_to portfolios_path
+    end
+
+    private
+
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body)
+    end
+
 
 end
