d6014eb12da119861c3e37019e5560abe802ab78
tylerstephens814 <tylerstephens814@gmail.com>
2017-01-17 15:09:51 -0700

    added contoller concerns for devise params

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,19 @@
+module DeviseWhitelist
+  extend ActiveSupport::Concern
+
+  included do
+
+  end
+    before_filter :configure_permitted_parameters, if: :devise_controller?
+  def 
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+end
+
+
+
+ 
