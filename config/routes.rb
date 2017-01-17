b6957452dc71908d473f6824a26895926689d214
tylerstephens814 <tylerstephens814@gmail.com>
2017-01-17 13:34:45 -0700

    added some authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: "portfolios#show", as: 'portfolio_show'
   
@@ -15,3 +16,5 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 end
+
+#, path '', path_names: {sign_in: 'login', sign_out: 'logout'}
