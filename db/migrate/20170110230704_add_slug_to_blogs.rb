ee4917f2a8e0891e463fab44a5c6f791b5a5a1da
tylerstephens814 <tylerstephens814@gmail.com>
2017-02-07 11:54:44 -0700

    Corrected syntax error

--- db/migrate/20170110230704_add_slug_to_blogs.rb
+++ db/migrate/20170110230704_add_slug_to_blogs.rb
@@ -1,4 +1,4 @@
-exitclass AddSlugToBlogs < ActiveRecord::Migration[5.0]
+class AddSlugToBlogs < ActiveRecord::Migration[5.0]
   def change
     add_column :blogs, :slug, :string
     add_index :blogs, :slug, unique: true
