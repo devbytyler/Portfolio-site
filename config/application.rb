0e10917ad861ae9c9ce203a16bfef0dd0e74aa4f
tylerstephens814 <tylerstephens814@gmail.com>
2017-02-07 11:42:41 -0700

    Updated secret key base

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ Bundler.require(*Rails.groups)
 module DevcampPortfolio
   class Application < Rails::Application
     config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
