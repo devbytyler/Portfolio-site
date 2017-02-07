ed6bf6713679fbb20eff6dfac899c3c307e3959d
tylerstephens814 <tylerstephens814@gmail.com>
2017-02-07 15:43:02 -0700

    Integrate production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://tylerstephens-portfolio.herokuapp.com', 'http://tylerstephens-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://tylerstephens-portfolio.herokuapp.com/cable"
+
 end
